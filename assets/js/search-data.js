1c091c9505e99cb0560e5d33072bc721e6992050
ma-kjh <132344612+ma-kjh@users.noreply.github.com>
2025-03-26 03:20:06 +0000

    Deploying to gh-pages from @ ma-kjh/ma-kjh.github.io@74d4cae6ca0e62b8a97ed3e18c3b68c2cc9ca344 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,18 +16,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "post-dive-into-tofu",
-      
-        title: "Dive into TOFU",
-      
-      description: "Code with me",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2025/TOFU-code/";
-        
-      },
-    },{id: "post-dive-into-clip",
+        },{id: "post-dive-into-clip",
       
         title: "Dive into CLIP",
       
